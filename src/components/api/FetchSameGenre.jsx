d968b41f0cd2b4c2860e0272c8ef5f413375d8b6
Rajesh Thapa <devrajeshthapa@gmail.com>
2024-09-05 19:20:26 +0545

    Fixed some issues

--- src/components/api/FetchSameGenre.jsx
+++ src/components/api/FetchSameGenre.jsx
@@ -63,7 +63,7 @@ const FetchSameGenre = ({ type, genre }) => {
 
                             {
                                 data.map((item, index) => (
-                                    <Link to={(type == "movie" ? "/movie/" : "tvseries") + item.id} className='contentWrapper cursor-pointer' key={index}>
+                                    <Link to={(type == "movie" ? "/movie/" : "/tvseries/") + item.id} className='contentWrapper cursor-pointer' key={index}>
                                         <div className='tag'>{item.adult == true ? "18+" : "HD"}</div>
                                         <img className='poster' src={"https://image.tmdb.org/t/p/w500/" + item.poster_path} alt="Poster not found!" />
                                         <div className='linearGradientBlack'></div>
