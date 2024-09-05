5c1efd9b905bf95877bde498e9d479089f1de1f8
Rajesh Thapa <devrajeshthapa@gmail.com>
2024-09-05 22:21:25 +0545

    Trailer added

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -10,11 +10,12 @@ const Watch = () => {
     let { type, id } = useParams();
     let [data, setData] = useState(null);
     let [genre, setGenre] = useState(null);
+    let [key, setKey] = useState(null);
 
     useEffect(() => {
         if (type === "movie") {
-            const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
-            const options = {
+            const url1 = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+            const options1 = {
                 method: 'GET',
                 headers: {
                     accept: 'application/json',
@@ -22,16 +23,35 @@ const Watch = () => {
                 }
             };
 
-            fetch(url, options)
+            fetch(url1, options1)
                 .then(res => res.json())
                 .then(json => {
                     setData(json);
                     setGenre("y")
+                })
+                .catch(err => console.error('error:' + err));
+
+
+            const url2 = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+            const options2 = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOThjYTk3Y2Y3YWJiNDk2OTJjNmVkOTIyOTIzNDc0MSIsIm5iZiI6MTcyNTU1MjA2Ni40MjkyMSwic3ViIjoiNjZhNzkxYTE1NTU0ODdjNTUwYjMxOGMzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Lb6GW65goylZRc2VpTe6YbRcFxxI5flEAFQCVoCqPV0'
+                }
+            };
+
+            fetch(url2, options2)
+                .then(res => res.json())
+                .then(json => {
+                    setKey(json.results.filter((item)=>{
+                        return item.type == "Trailer" || item.type == "Teaser";
+                    })[0].key);
                 })
                 .catch(err => console.error('error:' + err));
         } else {
-            const url = `https://api.themoviedb.org/3/tv/${id}?language=en-US`;
-            const options = {
+            const url1 = `https://api.themoviedb.org/3/tv/${id}?language=en-US`;
+            const options1 = {
                 method: 'GET',
                 headers: {
                     accept: 'application/json',
@@ -39,13 +59,32 @@ const Watch = () => {
                 }
             };
 
-            fetch(url, options)
+            fetch(url1, options1)
                 .then(res => res.json())
                 .then(json => {
                     setData(json);
                     setGenre("y")
                 })
                 .catch(err => console.error('error:' + err));
+
+            const url2 = `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`;
+            const options2 = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOThjYTk3Y2Y3YWJiNDk2OTJjNmVkOTIyOTIzNDc0MSIsIm5iZiI6MTcyNTU1MjA2Ni40MjkyMSwic3ViIjoiNjZhNzkxYTE1NTU0ODdjNTUwYjMxOGMzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.Lb6GW65goylZRc2VpTe6YbRcFxxI5flEAFQCVoCqPV0'
+                }
+            };
+
+            fetch(url2, options2)
+                .then(res => res.json())
+                .then(json => {
+                    setKey(json.results.filter((item)=>{
+                        return item.type == "Trailer" || item.type == "Teaser";
+                    })[0].key);
+                })
+                .catch(err => console.error('error:' + err));
+
         }
     }, [])
 
@@ -60,13 +99,16 @@ const Watch = () => {
 
                                 <div className='nowPlaying'>NOW PLAYING: <span>{data.name || data.original_name || data.title || data.original_title}</span></div>
                                 <div className="videoWrapper">
-                                    <video controls>
-                                        <source src={Movie} />
-                                    </video>
+                                    {
+                                        key ?
+                                            <iframe src={`https://www.youtube.com/embed/${key}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                            :
+                                            null
+                                    }
                                 </div>
                             </div>
                             <br />
-                            <div style={{display: "flex", flexDirection: "column", gap: "5px", width: '100%'}}>
+                            <div style={{ display: "flex", flexDirection: "column", gap: "5px", width: '100%' }}>
                                 <div className='contentTopRow'>
                                     <div className='font-bold text-lg'>YOU MAY ALSO LIKE</div>
                                 </div>
